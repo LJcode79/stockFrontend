538bf529c03be5c2fb83eaa60f34d87f25ac57cc
LJ <ljdb1999@gmail.com>
2025-03-21 14:44:07 -0700

    3/21/2025 working on getkeymetrics api fetch

--- src/Components/api.tsx
+++ src/Components/api.tsx
@@ -1,4 +1,4 @@
-import { CompanyProfile, CompanySearch } from "../company";
+import { CompanyKeyMetrics, CompanyProfile, CompanySearch } from "../company";
 import axios, { AxiosResponse, isAxiosError } from 'axios';
 
 interface SearchResponse {
@@ -36,4 +36,16 @@ export const getCompanyProfile = async (query: string) => {
     } catch (error : any) {
         console.log("error message from API: ", error.message);
     }
-}
+};
+
+export const getKeyMetrics = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyKeyMetrics[]>(
+            `https://financialmodelingprep.com/stable/key-metrics-ttm?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+        )
+        return data;
+    } catch (error : any) {
+        console.log("error message from API: ", error.message);
+    }
+};
+
--- src/Pages/DesignPage/DesignPage.tsx
+++ src/Pages/DesignPage/DesignPage.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import Table from '../../Components/Table/Table'
+import RatioList from '../../Components/RatioList/RatioList'
+import { TestDataCompany, testIncomeStatementData } from '../../Components/Table/testData'
 
 type Props = {}
+
+const data = TestDataCompany;
+
+const tableConfig = [
+  {
+    label: "Market Cap",
+    render: (company: any) => company.marketCapTTM,
+    subTitle: "Total value of all a company's shares of stock",
+  },
+]
 
 const DesignPage = (props: Props) => {
   return (
     <>
     <h1>FinShark Design Page</h1>
     <h2>This is Finshark's Design page. This is where we will house various design aspects of the app.</h2>
-    <Table/>
+    <RatioList data={testIncomeStatementData} config={tableConfig}/>
+    <Table />
     </>
   )
 }
